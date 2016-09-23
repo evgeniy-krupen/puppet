5bd76bfe078c6df1d3dc7ffcf54b8bf3f62c02ce
Evgeniy Krupen <evgeniy_krupen@epam.com>
2016-09-23 20:21:13 +0300

    template, fact

--- puppet.sh
+++ puppet.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 # this is script for web VM
 yum install -y puppet
-#rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
-#puppet apply /vagrant/puppet/exittask/manifest/init.pp
 echo "copy files"
-#cp -R /vagrant/puppet/exittask/ /root
 puppet apply -e 'include exittask' --modulepath=/vagrant/puppet/
