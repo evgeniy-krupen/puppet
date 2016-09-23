7e035e31dec998ef69cea0f925039ace55c56d09
Evgeniy Krupen <evgeniy_krupen@epam.com>
2016-09-23 15:53:27 +0300

    s

--- puppet.sh
+++ puppet.sh
@@ -4,5 +4,5 @@ yum install -y puppet
 #rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 #puppet apply /vagrant/puppet/exittask/manifest/init.pp
 echo "copy files"
-cp -R /vagrant/puppet/exittask/ /root
-puppet apply -e 'include exittask' --modulepath=/root
+#cp -R /vagrant/puppet/exittask/ /root
+puppet apply -e 'include exittask' --modulepath=/vagrant/puppet/
